bd735c1874e75bd667863dcd6ad0555742964882
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-13 20:13:37 +0700

    Add Structure Files

---
+++ server.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppetchda <ppetchda@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                              +#+#+#+#+#+     +#+           */
+/*   Created: 2022/03/13 20:13:59 by ppetchda          #+#    #+#             */
+/*   Updated: 2022/03/13 20:18:35 by ppetchda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+#include "libft/libft.h"
+
+int	main(void)
+{
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	return (0);
+}
