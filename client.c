18ba7d6ce6b7cac83c7011a77d50896a49c962f2
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-15 11:12:06 +0700

    Add More Good Code

--- client.c
+++ client.c
@@ -13,6 +13,46 @@
 #include <signal.h>
 #include "libft/libft.h"
 
+static void	action(int sig)
+{
+	static int	received = 0;
+
+	if (sig == SIGUSR1)
+		++received;
+	else
+	{
+		ft_putnbr_fd(received, 1);
+		ft_putchar_fd('\n', 1);
+		exit(0);
+	}
+}
+
+static void	mt_kill(int pid, char *str)
+{
+	int		i;
+	char	c;
+
+	while (*str)
+	{
+		i = 8;
+		c = *str++;
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc != 3 || !ft_strlen(argv[2]))
@@ -21,12 +61,13 @@ int	main(int argc, char **argv)
 		ft_putendl_fd("./client <process_id> <message>", 1);
 		return (1);
 	}
-		
 	ft_putstr_fd("Sent    : ", 1);
 	ft_putnbr_fd(ft_strlen(argv[2]), 1);
 	ft_putchar_fd('\n', 1);
 	ft_putstr_fd("Received: ", 1);
-
+	signal(SIGUSR1, action);
+	signal(SIGUSR2, action);
+	mt_kill(ft_atoi(argv[1]), argv[2]);
 	while (1)
 		pause();
 	return (0);
